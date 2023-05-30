ce0e7c14522b506cbbfe32208fe6530d4c2c2dd5
Alexis Marquez <alexis.marquez@agilethought.com>
2023-05-30 12:50:52 -0600

    Initial Commit

---
+++ BanKapp/BanKappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanKappApp.swift
+//  BanKapp
+//
+//  Created by Alexis Marquez on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BanKappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
